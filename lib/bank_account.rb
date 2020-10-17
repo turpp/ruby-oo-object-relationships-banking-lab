fe9785e34fe7545962cb5d696d16ff55fd94684f
turpp <white.tripp@icloud.com>
2020-10-17 12:22:47 -0600

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,42 @@
 class BankAccount
+attr_accessor :balance, :status
+attr_reader :name
+
+    def initialize(name,balance = 1000, status = "open")
+        @name=name
+        @balance = balance
+        @status = status
+    end
+
+    def deposit(amount)
+        self.balance=(@balance + amount)
+    end
+
+    def display_balance
+        "Your balance is $#{@balance}."
+    end
+
+    def valid?
+        #check the status and balance of the account
+        #if balance is > 0 true
+        #if status == open true
+        #if not the above then not valid
+        bal_good = @balance > 0
+        status_good = @status =="open"
+
+        if bal_good == status_good
+            true
+        else
+            false
+        end
+    end
+
+    def close_account
+        self.status=("closed")
+    end
+
+
+
+
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,78 @@
 class Transfer
   # your code here
+attr_accessor :sender, :receiver, :status, :amount
+  def initialize (sender, receiver, status = "pending", amount)
+    @sender = sender
+    @receiver = receiver
+    @status = status
+    @amount = amount
+  end
+
+
+
+  def valid?
+    #checks to see if both account are open
+    #checks account of the sender
+    #checks account of reciever
+    #if both are open - valid
+    #if one closed - not valid
+    #  binding.pry
+    # sender_status = @sender.status
+    # receiver_status = @receiver.status
+
+    # if sender_status == receiver_status
+    #   true 
+    # else
+    #   false
+    # end
+
+    #need call the valid method from the bank_account for both sender and reciever
+  if @sender.valid? == @receiver.valid?
+    true
+  else
+    false
+  end
+    
+
+
+  end
+
+  def reverse_transfer
+    #takes the transfer_amount
+    #reversed the execute_transaction to reciever sending to sender
+    if @status == "complete"
+    @receiver.deposit(-amount)
+    @sender.deposit(amount)
+    @status="reversed"
+    end
+  end
+
+
+  def execute_transaction
+      @execute_transaction ||= double_check
+  end
+
+  private
+  def double_check
+    
+    
+    if valid? && @sender.balance > amount
+      #---------------------------------
+      # the code is running more than once so the transaction happens more than once
+      # I need to make sure that this code only runs once. either private method or one of the other weird ways
+      #also need to run a second check to make sure that @sender balance is greater than @amount. 
+      #transfer balance from sender to balance of reciever
+# binding.pry
+      @sender.deposit(-@amount)
+      @receiver.deposit(@amount)
+      @status="complete"
+    else 
+      #reject
+      @status="rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+
+   
 end
